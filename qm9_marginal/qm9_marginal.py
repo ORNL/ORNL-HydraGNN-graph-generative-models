a5ce45bebc57e0c722f01833885a016ed8a7ba0c
bbhaduri <bbhaduri3@gatech.edu>
2024-07-26 14:22:20 -0400

    Point qm9 dataset to path in examples qm9 diffusion

--- qm9_marginal/qm9_marginal.py
+++ qm9_marginal/qm9_marginal.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
     dp = MarginalDiffusionProcess(args.diffusion_steps, marg_dist=get_marg_dist())
     train_tform = get_train_transform(dp, voi["type"], voi["output_index"], [], voi["output_dim"])
     dataset = torch_geometric.datasets.QM9(
-        root="qm9_dataset/raw", pre_transform=qm9_pre_transform, pre_filter=qm9_pre_filter, transform=train_tform)
+        root="../examples/qm9_diffusion/dataset/qm9", pre_transform=qm9_pre_transform, pre_filter=qm9_pre_filter, transform=train_tform)
     
     # NOTE: Commenting this section, fixes bug
     # datum = dataset[0]
